b250eb0e3a22a1f6ae20dbe7236b54e137f92920
Jyrki Keisala <jyrki.keisala@gmail.com>
2012-08-22 20:11:33 +0300

    Plugin configuration.

--- src/com/github/etsija/MagicTouch.java
+++ src/com/github/etsija/MagicTouch.java
@@ -1,7 +1,6 @@
 package com.github.etsija;
 
 import java.util.logging.Logger;
-
 import org.bukkit.plugin.Plugin;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -14,25 +13,30 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 
+
 public class MagicTouch extends JavaPlugin {
 
 	private Logger _log = Logger.getLogger("Minecraft");
 	
 	// Connect to WG plugin to respect build-restricted areas
 	private WorldGuardPlugin getWorldGuard() {
-	    Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
+		Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
 	 
-	    // WorldGuard may not be loaded
-	    if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
-	        return null; // Maybe you want throw an exception instead
-	    }
+		// WorldGuard may not be loaded
+		if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+			return null; // Maybe you want throw an exception instead
+		}
 	 
-	    return (WorldGuardPlugin) plugin;
+		return (WorldGuardPlugin) plugin;
 	}
 	
 	public void onEnable(){
 		// Register the listener for the tool to use
 		getServer().getPluginManager().registerEvents(new pListener(), this);
+		
+		// Create a default config.yml if one does not exist
+		saveDefaultConfig();
+		
 		_log.info("[MagicTouch] Has been enabled!");
 	}
 	 
@@ -44,6 +48,16 @@ public class MagicTouch extends JavaPlugin {
 	public class pListener implements Listener {
 
 		private boolean shiftClick = false;
+		
+		// Read configuration params from config.yml
+		private int magicTool = getConfig().getInt("general.magictool");
+		private Boolean useWG = getConfig().getBoolean("general.use_worldguard");
+		private Boolean useLB = getConfig().getBoolean("general.use_logblock");
+		private Boolean rotateLogs = getConfig().getBoolean("blocks.logs");
+		private Boolean rotateStairs = getConfig().getBoolean("blocks.stairs");
+		private Boolean rotatePistons = getConfig().getBoolean("blocks.pistons");
+		private Boolean rotateChests = getConfig().getBoolean("blocks.chests");
+		private Boolean rotateSlabs = getConfig().getBoolean("blocks.slabs");
 		
 		@EventHandler
 		public void onPlayerInteract(PlayerInteractEvent event) {
@@ -60,7 +74,7 @@ public class MagicTouch extends JavaPlugin {
 			Player player = event.getPlayer();
 			int itemInHand = player.getItemInHand().getTypeId();
 			
-			if (itemInHand == 58) {
+			if (itemInHand == magicTool) {
 				
 				// Did the player shift-click?
 				shiftClick = player.isSneaking();
@@ -69,9 +83,11 @@ public class MagicTouch extends JavaPlugin {
 				Material material = block.getType();
 				
 				// Can you actually build in this area? Let WG decide
-				if (!getWorldGuard().canBuild(player, block)) {
-					player.sendMessage(ChatColor.RED + "[MagicTouch] You cannot build in this area!");
-					return;
+				if (useWG) {
+					if (!getWorldGuard().canBuild(player, block)) {
+						player.sendMessage(ChatColor.RED + "[MagicTouch] You cannot build in this area!");
+						return;
+					}
 				}
 				
 				//_log.info("Type = " + material);
@@ -82,7 +98,8 @@ public class MagicTouch extends JavaPlugin {
 				
 				// LOGS:
 				// U/D -> E/W -> N/S -> U/D...
-				if (material == Material.LOG) {
+				if (rotateLogs && 
+				   (material == Material.LOG)) {
 					byte data = block.getData();
 					if (data < 4) {
 						block.setData((byte)(data + 4));
@@ -95,15 +112,16 @@ public class MagicTouch extends JavaPlugin {
 				// STAIRS: 
 				// N(3) -> E(0) -> S(2) -> W(1) -> N(3)...
 				// With shift-click, flip stairs upside down
-				} else if ((material == Material.WOOD_STAIRS) ||
-						   (material == Material.SPRUCE_WOOD_STAIRS) ||
-						   (material == Material.BIRCH_WOOD_STAIRS) ||
-						   (material == Material.JUNGLE_WOOD_STAIRS) ||
-						   (material == Material.COBBLESTONE_STAIRS) ||
-						   (material == Material.SMOOTH_STAIRS) ||
-						   (material == Material.SANDSTONE_STAIRS) ||
-						   (material == Material.BRICK_STAIRS) ||
-						   (material == Material.NETHER_BRICK_STAIRS)) {
+				} else if (rotateStairs && (
+						  (material == Material.WOOD_STAIRS) ||
+						  (material == Material.SPRUCE_WOOD_STAIRS) ||
+						  (material == Material.BIRCH_WOOD_STAIRS) ||
+						  (material == Material.JUNGLE_WOOD_STAIRS) ||
+						  (material == Material.COBBLESTONE_STAIRS) ||
+						  (material == Material.SMOOTH_STAIRS) ||
+						  (material == Material.SANDSTONE_STAIRS) ||
+						  (material == Material.BRICK_STAIRS) ||
+						  (material == Material.NETHER_BRICK_STAIRS))) {
 					byte data = block.getData();
                     int flipStatus = (data & 0x4) >> 2; // Save bit 3 (0x3)
 					int orientation = data & 0x3;
@@ -131,11 +149,11 @@ public class MagicTouch extends JavaPlugin {
                     data = (byte) ((flipStatus << 2) | orientation);
 					block.setData(data);
 
-/* Disabled for now
 				// PISTONS:
 				// N(2) -> E(5) -> S(3) -> W(4) -> U(1) -> D(0) -> N(2)...
-				} else if ((material == Material.PISTON_BASE) ||
-						   (material == Material.PISTON_STICKY_BASE)) {
+				} else if (rotatePistons && (
+						  (material == Material.PISTON_BASE) ||
+						  (material == Material.PISTON_STICKY_BASE))) {
 					byte data = block.getData();
 					int isExtended = (data & 0x8) >> 3; // Save bit 4 (0x8) = extension status
 					int orientation = data & 0x7;
@@ -162,13 +180,14 @@ public class MagicTouch extends JavaPlugin {
 					// Combine extension status (bit 4) and orientation (bits 3-0)
 					data = (byte) ((isExtended << 3) | orientation);
 					block.setData(data);
-*/					
+		
 				// CHESTS, FURNACES, DISPENSERS:
 				// N(2) -> E(5) -> S(3) -> W(4) -> N(2)...
-				} else if ((material == Material.CHEST) ||
-						   (material == Material.ENDER_CHEST) ||
-						   (material == Material.FURNACE) ||
-						   (material == Material.DISPENSER)) {
+				} else if (rotateChests && (
+						  (material == Material.CHEST) ||
+						  (material == Material.ENDER_CHEST) ||
+						  (material == Material.FURNACE) ||
+						  (material == Material.DISPENSER))) {
 					byte data = block.getData();
 					switch (data) {
 						case 2:
@@ -187,7 +206,7 @@ public class MagicTouch extends JavaPlugin {
 				
 				// SLABS:
 				// right-side up -> upside down -> ...
-				} else if (material == Material.STEP) {
+				} else if (rotateSlabs && (material == Material.STEP)) {
 					byte data = block.getData();
 					if (data < 8) {
 						block.setData((byte)(data + 8));
